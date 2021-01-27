634575a821d9a00db1053456500e0985644c64fe
Joaquin Osio <joaquinosio1@gmail.com>
2021-01-26 18:34:47 -0600

    Upload of the initial chart and values

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,45 +14,62 @@ function makeResponsive() {
     }
 
     // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    var margin = {top: 10, right: 30, bottom: 30, left: 60}
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     var svg = d3.select("#scatter")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", 960)
+    .attr("height", 500)
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
     
     //Read the data
-    d3.csv("assets/data/data.csv", function(data) {
+    d3.csv("assets/data/data.csv").then(function(data) {
     
         // Add X axis
-        var x = d3.scaleLinear()
-            .domain([0, 4000])
-            .range([ 0, width ]);
-        svg.append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(d3.axisBottom(x));
+        var xLinearScale = d3.scaleLinear()
+            .domain([30,
+                d3.max(data, d => d.age * 1.2) 
+            ])
+            .range([0, width]);
+
+        // svg.append("g")
+        //     .attr("transform", `translate(0, ${height})`)
+        //     // .call(d3.axisBottom(xLinearScale));
         
         // Add Y axis
-        var y = d3.scaleLinear()
-            .domain([0, 500000])
-            .range([ height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
+        var yLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => +d.healthcare)]) 
+            .range([height, 0]);
+
+        // svg.append("g")
+        //     .call(d3.axisLeft(yLinearScale));
         
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+
+        // append x axis
+        var xAxis = svg.append("g")
+            .classed("x-axis", true)
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+        
+        // append y axis
+        svg.append("g")
+            .call(leftAxis);
+
         // Add dots
-        svg.append('g')
-            .selectAll("dot")
+        svg
+            .selectAll("g circle")
             .data(data)
             .enter()
             .append("circle")
-                .attr("cx", function (d) { return x(d.age); } )
-                .attr("cy", function (d) { return y(d.healthcare); } )
-                .attr("r", 1.5)
+                .attr("cx", function (d) { return xLinearScale(d.age); } )
+                .attr("cy", function (d) { return yLinearScale(d.healthcare); } )
+                .attr("r", 10)
                 .style("fill", "#69b3a2")
 })
 }
